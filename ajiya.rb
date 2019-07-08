d9f90e5e414f09b9f82c7337c4d0105f8480097f
井澤諒哉 <izawa@izawaryoyanoMacBook-puro.local>
2019-07-08 19:29:37 +0900

    update

--- ajiya.rb
+++ ajiya.rb
@@ -3,30 +3,35 @@ require 'json'
 require 'nokogiri'
 require 'open-uri'
 
-url=URI::parse('http://ajiya1.com/')
-page=url.read()
-doc=Nokogiri::HTML::parse(page)
+ajiya_uri = 'http://ajiya1.com/'
+ajiya_page = URI::parse(ajiya_uri).read()
+ajiya_doc = Nokogiri::HTML::parse(ajiya_page)
 
-ajiya={タイトル:'.rpwe-title'}
-ajiya.each do |key,value|
-    puts("[#{key}]")
-    tmp=doc.css('.rpwe-block').css(value)
-    el=doc.css('.rpwe-block').css('h3')
-    if(tmp.size===0)then
-        puts("今日はお休みです")
-    else
-        tmp.each do |item|
-            s="#{item.content}"
-            puts s
-            token='xoxb-233137244498-532173993376-IN6zzq9F3pTMYueOGIp2STBV'
-            response=HTTP.post("https://slack.com/api/chat.postMessage",params:{
-            token:token,
-            channel:"DFNSTFKNE",
-            text:s,
-            as_user: true
-        })
-        puts JSON.pretty_generate(JSON.parse(response.body))
-        end
-    end
-    puts(" ")
-end
+dairy_uri = ajiya_doc.css('.rpwe-block').css('.rpwe-title').css('a')[0][:href]
+# puts dairy_uri
+
+dairy_page = URI::parse(dairy_uri).read()
+dairy_doc = Nokogiri::HTML::parse(dairy_page)
+
+bento = dairy_doc.css('.entry-content')
+bento_str = bento.to_html.gsub(%r{</?[^>]+?>},'').strip.gsub('●','').chomp
+
+puts bento_str
+
+    # if(tmp.size===0)then
+    #     puts("今日はお休みです")
+    # else
+    #     tmp.each do |item|
+    #         s="#{item.content}"
+    #         puts s
+    #         token='xoxb-233137244498-532173993376-IN6zzq9F3pTMYueOGIp2STBV'
+    #         response=HTTP.post("https://slack.com/api/chat.postMessage",params:{
+    #         token:token,
+    #         channel:"DFNSTFKNE",
+    #         text:s,
+    #         as_user: true
+    #     })
+    #     puts JSON.pretty_generate(JSON.parse(response.body))
+    #     end
+    # end
+    # puts(" ")
--- bot.rb
+++ bot.rb
@@ -3,7 +3,8 @@ require 'json'
 require 'slack'
 
 Slack.configure do |config|
-    config.token = 'xoxb-233137244498-532173993376-IN6zzq9F3pTMYueOGIp2STBV'
+    config.token = ''
+    # xoxb-233137244498-532173993376-IN6zzq9F3pTMYueOGIp2STBV
 end
   
 text = "あじやテスト"
